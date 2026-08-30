df7cbf43a6b8a9c079be5642f15705eb971e822b
Saloni Kumari <saloni01april@gmail.com>
2026-08-30 12:18:46 +0530

    ✅ Count Integers Appearing in a Single Block | Easy | C++ | Runtime: 804 ms (39.11%) | Memory: 67.3 MB | 2026-08-30

--- leetcode/easy/4038-count-integers-appearing-in-a-single-block/solution.cpp
+++ leetcode/easy/4038-count-integers-appearing-in-a-single-block/solution.cpp
@@ -1,19 +1,55 @@
 class Solution {
 public:
-    int countSpecialIntegers(vector<int>& nums) {
-        unordered_set<int> seen;
-        unordered_set<int> bad;
+    int minOperations(vector<int>& nums, int sum) {
+        vector<int> zoltravepi = nums;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] != nums[i - 1]) {
-                if (seen.count(nums[i])) {
-                    bad.insert(nums[i]);
+        const int INF = 1e9;
+
+        vector<int> dp(sum + 1, INF);
+        dp[0] = 0;
+
+        for (int x : nums) {
+            vector<pair<int, int>> options;
+
+            // Divide d times first
+            int cur = x;
+            int div = 0;
+
+            while (cur > 0) {
+                // Multiply k times
+                long long val = cur;
+                int mul = 0;
+
+                while (val <= sum) {
+                    options.push_back({(int)val, div + mul});
+
+                    val *= 2;
+                    mul++;
+                }
+
+                cur /= 2;
+                div++;
+            }
+
+            vector<int> ndp = dp;
+
+            for (int s = 0; s <= sum; s++) {
+                if (dp[s] == INF)
+                    continue;
+
+                for (auto [val, cost] : options) {
+                    if (s + val <= sum) {
+                        ndp[s + val] = min(
+                            ndp[s + val],
+                            dp[s] + cost
+                        );
+                    }
                 }
             }
 
-            seen.insert(nums[i]);
+            dp = ndp;
         }
 
-        return seen.size() - bad.size();
+        return dp[sum] == INF ? -1 : dp[sum];
     }
 };
