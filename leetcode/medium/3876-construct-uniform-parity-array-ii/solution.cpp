854df99f98f683a53942b3a1becff7881c8b6369
Saloni Kumari <saloni01april@gmail.com>
2026-09-03 18:49:21 +0530

    ✅ Construct Uniform Parity Array II | Medium | C++ | Runtime: 1 ms (79.58%) | Memory: 165.8 MB | 2026-09-03

--- leetcode/medium/3876-construct-uniform-parity-array-ii/solution.cpp
+++ leetcode/medium/3876-construct-uniform-parity-array-ii/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int mnOdd = INT_MAX;
+        int smallestOdd = INT_MAX;
 
         for (int x : nums1) {
-            if (x % 2)
-                mnOdd = min(mnOdd, x);
+            if (x % 2 != 0) {
+                smallestOdd = min(smallestOdd, x);
+            }
         }
+        if (smallestOdd == INT_MAX)
+            return true;
 
         for (int x : nums1) {
-            if (x % 2 == 0) {
-                if (mnOdd >= x)
-                    return false;
+            if (x % 2 == 0 && x <= smallestOdd) {
+                return false;
             }
         }
 
