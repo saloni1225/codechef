00b3959691f631d15f73f660106008f732779425
Saloni Kumari <saloni01april@gmail.com>
2026-09-11 14:24:56 +0530

    ✅ Check for Binary String | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-09-11

--- gfg/basic/check-for-binary/solution.cpp
+++ gfg/basic/check-for-binary/solution.cpp
@@ -2,10 +2,7 @@ class Solution {
   public:
     bool isBinary(string& s) {
     for(int i=0;i<s.length();i++){
-        if(s[i]=='0' || s[i]=='1'){
-            continue;
-        }
-        else{
+        if(s[i]!='0' && s[i]!='1'){
             return false;
         }
     }
